875f97f021eea2f597b481e0911a1e85633bebd8
YuShx01 <coveringguy0123@gmail.com>
2023-01-01 19:59:17 +0545

    Combinational Sum - 3 | Microsoft Sheet | Did the qn in one go with no issues.😎

--- Microsoft/CombinationSum3.cpp
+++ Microsoft/CombinationSum3.cpp
@@ -2,8 +2,37 @@
 using namespace std;
 
 class Solution {
-public:
+  public:
+    vector<vector<int>> ans;
+    void findSum(vector<int> &curr, int k, int n, vector<bool> &taken, int i) {
+
+        if (i == 10) {
+            if (k == 0 && n == 0) {
+                ans.push_back(curr);
+                return;
+            }
+            return;
+        }
+
+        // if we can take.
+        if (i <= n && taken[i] == 0 && k > 0) {
+
+            curr.push_back(i);
+            taken[i] = 1;
+            findSum(curr, k - 1, n - i, taken, i + 1);
+            taken[i] = 0;
+            curr.pop_back();
+        }
+
+        // not take.
+        findSum(curr, k, n, taken, i + 1);
+    }
+
     vector<vector<int>> combinationSum3(int k, int n) {
-        
+
+        vector<int> curr;
+        vector<bool> taken(10, 0);
+        findSum(curr, k, n, taken, 1);
+        return ans;
     }
 };
